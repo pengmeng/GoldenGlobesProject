21bb71a4d950a2c6394a5b4a24056c64282bbe7e
Shiv <raishiv@gmail.com>
2015-02-10 00:51:59 -0600

    red carpet basic done

--- ggp/ggnew.py
+++ ggp/ggnew.py
@@ -47,8 +47,7 @@ Award_Categories = ["Best Motion Picture - Drama",
 "Best Performance by an Actor in a Supporting Role in a Series, Mini-Series or Motion Picture Made for Television",
 "Best Performance by an Actress in a Supporting Role in a Series, Mini-Series or Motion Picture Made for Television",
 "Best Mini-Series or Motion Picture Made for Television",
-"Cecil B. DeMille Award",
-"Host of the Show"]
+"Cecil B. DeMille Award"]
 
 
 
@@ -77,14 +76,17 @@ Category_keywords = [["best picture","drama"],
 ["best supporting actor","tv"],
 ["best supporting actress","tv"],
 ["best miniseries","tv"],
-["cecil","award"],
-["golden","hosts"]]
+["cecil","award"]]
 
 Num_of_Category = len(Award_Categories)
+NUMBER_OF_BEST_DRESSED = 5
+NUMBER_OF_PRESENTERS = 26
+BIGRAM_RE = "([A-Z][a-z]+\s[A-Z][-'a-zA-Z]+)"
 
-remove_keywords = ["best","picture" , "actor","actress","hosts","hosting","drama","golden","globes","movie","director","song","original"
-,"foreign","film","tv","series","mini","supporting","screenplay","presenting","presented","musical",
-"comedy","motion","animated","feature","wins","winner","won","cecil", "b", "demille","goldenglobes","score","miniseries","award"]
+remove_keywords = ["best","picture" , "actor","actress","hosts","hosting","drama","golden","globes",
+"movie","director","song","original","foreign","film","tv","series","mini","supporting","screenplay",
+"presenting","presented","musical","comedy","motion","animated","feature","wins","winner","won","cecil",
+ "demille","goldenglobes","score","miniseries","award","dressed","dress","b"]
 
 Nominees = dict()
 
@@ -109,12 +111,10 @@ def get_nominees(index):
     return Nominees
     #pdb.set_trace()
 
+
+
 def create_useful_tweet_bank():
-	word = ["winner","wins","hosts","presenting","won","hosting"]
-	words = ["hosting","best actor", "best actress", "best supporting","best picture", "best movie","best director",
-	"best original song", "best foreign film",
-	 "best original score","best animated", "tv series",
-	  "best screenplay","mini series","Cecil B. DeMille Award","presenting","presented"]
+	word = ["winner","wins","hosts","presenting","won","hosting","dress","dressed"]
 	count = 0
 	file = open("newfile.txt", "w")
 	for t in tweet_text:
@@ -181,29 +181,165 @@ def find_winners(category,keywords):
 	file.close()
 	if temp_dict: 
 		abc = max(temp_dict, key = temp_dict.get)
-		if (category != "Cecil B. DeMille Award") and (category != "Host of the Show"):
+		if (category != "Cecil B. DeMille Award"):
 			name = get_fullName(abc,category)
 			#pdb.set_trace()
-		if (category == "Host of the Show"):
-			print (category +":"+"%s %s"%abc)
-		elif(category == "Cecil B. DeMille Award"):
+		
+		if(category == "Cecil B. DeMille Award"):
 			print (category+": "+str(abc))
 		else:
 			print (category+": "+str(name))
-	if category == "Host of the Show":
-		temp_dict.pop(abc, None)
-		xyz = max(temp_dict, key = temp_dict.get)
-		print ("and "+"%s %s"%xyz)
-		
 	
+
+
+
+		
+def find_best_dressed():
+    # This function will find the best dressed celebrities of the show.
+    results = dict()
+    for tweet in new_tweets:
+        possible_winners = list()
+        tt = tweet.lower()
+        host_words = ["best dressed", "best dress"]
+        for w in host_words:
+            if w in tt:
+                possible_winners = possible_winners + re.findall(BIGRAM_RE, tweet)
+        for r in possible_winners:
+            if r in results:
+                results[r] += 1
+            else:
+                results[r] = 1
+    results.pop("Golden Globes")
+    results.pop("Best Dressed")
+    i = 0
+    best_dressed = list()
+    while results and (i<NUMBER_OF_BEST_DRESSED):
+        next = max(results, key = results.get);
+        best_dressed.append(next);
+        results.pop(next);
+        i += 1
+    return best_dressed
+
+
+
+
+
+def find_worst_dressed():
+    # This function will find the worst dressed celebrities of the show.
+    results = dict()
+    for tweet in new_tweets:
+        possible_winners = list()
+        tt = tweet.lower()
+        host_words = ["worst dressed", "worst dress"]
+        for w in host_words:
+            if w in tt:
+                possible_winners = possible_winners + re.findall(BIGRAM_RE, tweet)
+        for r in possible_winners:
+            if r in results:
+                results[r] += 1
+            else:
+                results[r] = 1
+    results.pop("Golden Globes")
+    results.pop("Worst Dressed")
+    results.pop("Best Dressed")
+    i = 0
+    worst_dressed = list()
+    while results and (i<NUMBER_OF_BEST_DRESSED):
+        next = max(results, key = results.get);
+        worst_dressed.append(next);
+        results.pop(next);
+        i += 1
+    return worst_dressed
+
+def find_host():
+    # This function will find the worst dressed celebrities of the show.
+    results = dict()
+    for tweet in new_tweets:
+        possible_winners = list()
+        tt = tweet.lower()
+        host_words = ["host", "hosting","hosts","hosted"]
+        for w in host_words:
+            if w in tt:
+                possible_winners = possible_winners + re.findall(BIGRAM_RE, tweet)
+        for r in possible_winners:
+            if r in results:
+                results[r] += 1
+            else:
+                results[r] = 1
+    results.pop("Golden Globes")
+    i = 0
+    host = list()
+    while results and (i<2):
+        next = max(results, key = results.get);
+        host.append(next);
+        results.pop(next);
+        i += 1
+    return host
+
+def find_presenters():
+    # This function will find the presenters of the show.
+    results = dict()
+    stopwords = nltk.corpus.stopwords.words('english')
+    for tweet in new_tweets:
+        possible_winners = list()
+        tt = tweet.lower()
+        host_words = ["presenting", "presented"]
+        tn = re.sub(r'[^a-zA-Z0-9 ]',r'',tweet)
+        stopwords = nltk.corpus.stopwords.words('english')
+        word_list = tn.split()
+        res = ' '.join([i for i in word_list if i.lower() not in stopwords])
+        for w in host_words:
+            if w in tt:
+                possible_winners = possible_winners + re.findall(BIGRAM_RE, res)
+        for r in possible_winners:
+            if r in results:
+                results[r] += 1
+            else:
+                results[r] = 1
+    #results.pop("Golden Globes")
+    remove_words = ["presenting","presented","golden","globes"]
+    for key in results:
+    	for w in remove_words:
+    		if (w in key.lower()) or (key.lower() in stopwords):
+    			results[key] = 0
+
+
+    i = 0
+    presenters = list()
+    while results and (i<NUMBER_OF_PRESENTERS):
+        next = max(results, key = results.get);
+        presenters.append(next);
+        results.pop(next);
+        i += 1
+    return presenters
 
 
 
 def main():
 	
 	create_useful_tweet_bank()
-	print "\n\n### WINNERS & HOSTS ### \n"
+	print("\n\n********  HOSTS OF THE SHOW  ***************")
+	hosts = find_host()
+	for a in range(0,2,1):
+		print(str(hosts[a]))
+	print "\n\n***************  WINNERS  *******************\n"
 	get_winners()
+	presenter = find_presenters()
+	print("\n\n***********  PRESENTERS LIST ************")
+	for k in range(0,len(presenter),1):
+		print(str(presenter[k]))
+	best_dress = find_best_dressed()
+	print("\n*************  FUN GOALS  *****************\n")
+	print ("\n\n********  BEST DRESSED CELEBS  ************")
+	for i in range(0,len(best_dress),1):
+		print(str(best_dress[i]))
+	worst_dress = find_worst_dressed()
+	print ("\n\n********   WORST DRESSED CELEBS   ***********")
+	for j in range(0,len(worst_dress),1):
+		print(str(worst_dress[j]))
+
+	
+
 	#find_winners(Award_Categories[19],Category_keywords[19])
 	
 	#pdb.set_trace()
