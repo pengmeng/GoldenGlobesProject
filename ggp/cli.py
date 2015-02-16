3cfe6328a4afd0ea1fc76b94cf15cb878523491e
pengmeng <mengp3157@gmail.com>
2015-02-16 10:05:03 -0600

    support args and submenu in cli

--- ggp/cli.py
+++ ggp/cli.py
@@ -9,20 +9,25 @@ class CLI(object):
         self.menu = OrderedDict({'description': description})
         self.menu['exit'] = 'exit'
 
-    def register(self, name, func):
-        self.menu[str(len(self.menu) - 1)] = (name, func)
+    def register(self, name, func, *args):
+        self.menu[str(len(self.menu) - 1)] = (name, func, args)
 
     def exitcomm(self, comm):
         self.menu['exit'] = comm
 
     def show(self):
+        self._print()
+        while self.execfunc():
+            self._print()
+
+    def _print(self):
         for k, v in self.menu.iteritems():
             if k is 'description':
                 print(v)
+                print('-'*len(v))
             elif k is not 'exit':
                 print('{0}:   {1}'.format(k, v[0]))
-        print('{0}:   to exit program'.format(self.menu['exit']))
-        return self.execfunc()
+        print('{0}:   to exit menu'.format(self.menu['exit']))
 
     def execfunc(self):
         num = raw_input('Please enter menu #: ')
@@ -30,13 +35,13 @@ class CLI(object):
             print('Exiting...')
             return False
         if num in self.menu:
-            self.menu[num][1](num, self.menu[num])
+            self.menu[num][1](*self.menu[num][2])
         else:
             print('#{0} is not in the menu.'.format(num))
         return True
 
     def samplefunc(self, *args):
-        print('#{0} is selected and {1} is executed.'.format(args[0], args[1][1]))
+        print('#{0} is selected and {1} is executed.'.format(*args))
 
 
 if __name__ == '__main__':
